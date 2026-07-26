224cd8b33369e839d26f8452cf7e52d295b370c2
MohammedThaher01 <mdthaher2005@gmail.com>
2026-07-26 15:31:37 +0530

    fix: switch to llama-3.1-8b-instant for higher token limit

--- pipeline/scorer.py
+++ pipeline/scorer.py
@@ -15,7 +15,7 @@ from pipeline.ranker import rank_eligible_jobs
 
 logger = logging.getLogger(__name__)
 
-MODEL = "llama-3.3-70b-versatile"
+MODEL = "llama-3.1-8b-instant"
 REQ_INTERVAL = 5.0
 _last_call_ts = 0.0
 
